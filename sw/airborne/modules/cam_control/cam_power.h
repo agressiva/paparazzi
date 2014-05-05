42a7e61919a7942a169167866a91b04b8759020f
agressiva <agressiva@hotmail.com>
2014-05-05 19:18:30 -0300

    new file:   conf/airframes/AGR/quad_450.xml 	new file:   conf/flight_plans/rotorcraft_propeller.xml 	new file:   conf/flight_plans/rotorcraft_propeller_curto.xml 	new file:   conf/flight_plans/rotorcraft_propeller_no_sonar.xml 	new file:   conf/modules/cam_power.xml 	new file:   conf/modules/krooz_cam.xml 	new file:   conf/radios/TURNIGY_PPM.xml 	new file:   sw/airborne/modules/cam_control/cam_power.c 	new file:   sw/airborne/modules/cam_control/cam_power.h 	new file:   sw/airborne/modules/cam_control/krooz_cam.c 	new file:   sw/airborne/modules/cam_control/krooz_cam.h

---
+++ sw/airborne/modules/cam_control/cam_power.h
@@ -0,0 +1,121 @@
+#ifndef KAMERA_H_INCLUDED
+#define KAMERA_H_INCLUDED
+
+#include "std.h"
+#include BOARD_CONFIG
+
+#ifdef LPC21
+#ifndef ADC_CHANNEL_CAM1
+#define ADC_CHANNEL_CAM1 		ADC_7
+#endif
+
+#define CAM1_STATE_ON_LEVEL 	512
+#define CAM2_STATE_ON_LEVEL 	512
+
+#define VIDEO
+
+#ifndef CAM1_SHOT_PORT
+#define CAM1_SHOT_PORT 		0
+#endif
+#ifndef CAM1_SHOT_PIN
+#define CAM1_SHOT_PIN 			12
+#endif
+#ifndef CAM1_SWITCH_PORT
+#define CAM1_SWITCH_PORT 		0
+#endif
+#ifndef CAM1_SWITCH_PIN
+#define CAM1_SWITCH_PIN 		7
+#endif
+#ifndef CAM1_VIDEO_PORT
+#define CAM1_VIDEO_PORT 		0
+#endif
+#ifndef CAM1_VIDEO_PIN
+#define CAM1_VIDEO_PIN 		13
+#endif
+
+#ifndef CAM1_PINSEL
+#define CAM1_PINSEL 			PINSEL0
+#endif
+
+#ifndef CAM1_PINSEL_VAL
+#define CAM1_PINSEL_VAL 		(3<<24)
+#endif
+
+#ifdef ADC_CHANNEL_CAM1
+//struct adc_buf cam1_state;
+#endif
+#ifdef CAMERA_2
+#ifdef ADC_CHANNEL_CAM2
+struct adc_buf cam2_state;
+#endif
+#endif
+
+#define CAM1_SHOT_IODIR IO_(CAM1_SHOT_PORT, DIR)
+#define CAM1_SHOT_IOSET IO_(CAM1_SHOT_PORT, SET)
+#define CAM1_SHOT_IOCLR IO_(CAM1_SHOT_PORT, CLR)
+#define CAM1_SHOT_IOPIN IO_(CAM1_SHOT_PORT, PIN)
+
+#define CAM1_SWITCH_IODIR IO_(CAM1_SWITCH_PORT, DIR)
+#define CAM1_SWITCH_IOSET IO_(CAM1_SWITCH_PORT, SET)
+#define CAM1_SWITCH_IOCLR IO_(CAM1_SWITCH_PORT, CLR)
+#define CAM1_SWITCH_IOPIN IO_(CAM1_SWITCH_PORT, PIN)
+
+#define CAM1_VIDEO_IODIR IO_(CAM1_VIDEO_PORT, DIR)
+#define CAM1_VIDEO_IOSET IO_(CAM1_VIDEO_PORT, SET)
+#define CAM1_VIDEO_IOCLR IO_(CAM1_VIDEO_PORT, CLR)
+#define CAM1_VIDEO_IOPIN IO_(CAM1_VIDEO_PORT, PIN)
+
+#ifdef CAMERA_2
+#define CAM2_SHOT_IODIR IO_(CAM2_SHOT_PORT, DIR)
+#define CAM2_SHOT_IOSET IO_(CAM2_SHOT_PORT, SET)
+#define CAM2_SHOT_IOCLR IO_(CAM2_SHOT_PORT, CLR)
+#define CAM2_SHOT_IOPIN IO_(CAM2_SHOT_PORT, PIN)
+
+#define CAM2_SWITCH_IODIR IO_(CAM2_SWITCH_PORT, DIR)
+#define CAM2_SWITCH_IOSET IO_(CAM2_SWITCH_PORT, SET)
+#define CAM2_SWITCH_IOCLR IO_(CAM2_SWITCH_PORT, CLR)
+#define CAM2_SWITCH_IOPIN IO_(CAM2_SWITCH_PORT, PIN)
+#endif
+
+#define Cam1Clr()	{CAM1_PINSEL &= ~CAM1_PINSEL_VAL; CAM1_SHOT_IODIR |= (1 << CAM1_SHOT_PIN); CAM1_SHOT_IOCLR = (1 << CAM1_SHOT_PIN);}
+#define Cam1Set()	{CAM1_SHOT_IODIR &= ~(1 << CAM1_SHOT_PIN); CAM1_SHOT_IOSET = (1 << CAM1_SHOT_PIN); CAM1_PINSEL |= CAM1_PINSEL_VAL;}
+#define Cam1SwitchSet() {CAM1_SWITCH_IODIR |= (1 << CAM1_SWITCH_PIN); CAM1_SWITCH_IOSET = (1 << CAM1_SWITCH_PIN);}
+#define Cam1SwitchClr() {CAM1_SWITCH_IODIR |= (1 << CAM1_SWITCH_PIN); CAM1_SWITCH_IOCLR = (1 << CAM1_SWITCH_PIN);}
+#define Cam1VideoSet() {CAM1_VIDEO_IODIR |= (1 << CAM1_VIDEO_PIN); CAM1_VIDEO_IOSET = (1 << CAM1_VIDEO_PIN);}
+#define Cam1VideoClr() {CAM1_VIDEO_IODIR |= (1 << CAM1_VIDEO_PIN); CAM1_VIDEO_IOCLR = (1 << CAM1_VIDEO_PIN);}
+#endif
+
+#ifdef STM32F4
+#define CAM_STATE_ON_LEVEL 	2048
+#define CamClr()	{gpio_mode_setup(CAM_SH_GPIO, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, CAM_SH_GPIO_PIN); gpio_clear(CAM_SH_GPIO, CAM_SH_GPIO_PIN);}
+#define CamSet()	{gpio_set(CAM_SH_GPIO, CAM_SH_GPIO_PIN); gpio_mode_setup(CAM_SH_GPIO, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, CAM_SH_GPIO_PIN);}
+#define Cam1SwitchClr() {gpio_clear(CAM_SW_GPIO, CAM_SW_GPIO_PIN);}
+#define Cam1SwitchSet() {gpio_set(CAM_SW_GPIO, CAM_SW_GPIO_PIN);}
+#define Cam1VideoSet() {gpio_set(CAM_V_GPIO, CAM_V_GPIO_PIN);}
+#define Cam1VideoClr() {gpio_clear(CAM_V_GPIO, CAM_V_GPIO_PIN);}
+#endif
+
+#define Cam1Check() (cam1_state.sum / cam1_state.av_nb_sample > CAM_STATE_ON_LEVEL)
+//#define Cam1Check() TRUE
+#define DO_SHOTS 	(radio_control.values[RADIO_SHOTS] > MAX_PPRZ / 2)
+#define SHOT_VIDEO 	(radio_control.values[RADIO_PH_VD] > MAX_PPRZ / 2)
+
+extern bool_t   cam_shot;
+extern bool_t   cam_alt_on;
+extern uint16_t  kamera_interval;
+extern int32_t  cam_min_alt;
+
+void kamera_check_gps(void);
+void kamera_init( void );
+extern void periodic_task_kamera(void);
+
+extern uint16_t last_known_shot;
+extern uint8_t shot_period;
+extern uint8_t stage_photo_num;
+
+extern int16_t cam1_state_val;
+
+#ifndef SITL
+void CacheDel(uint16_t photo_nr);
+#endif
+#endif
---
+++ sw/airborne/modules/cam_control/krooz_cam.c
@@ -0,0 +1,142 @@
+/*
+ * $Id: $
+ *
+ * Copyright (C) 2012 Sergey Krukowski <softsr@yahoo.de>
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "cam_control/krooz_cam.h"
+//#include "cam_control/cam_power.h"
+#include "state.h"
+#include "subsystems/commands.h"
+//#include "subsystems/ahrs.h"
+#include "subsystems/ins.h"
+#include "generated/flight_plan.h"
+#include "subsystems/radio_control.h"
+#include "subsystems/navigation/common_nav.h"
+#include "subsystems/actuators/actuators_pwm_arch.h"
+#include "subsystems/actuators/actuators_pwm.h"
+
+#ifndef TILT_COEFF
+#define TILT_COEFF 500
+#endif
+#ifndef PAN_COEFF
+#define PAN_COEFF 500
+#endif
+#ifndef TILT_RATE
+#define TILT_RATE 50
+#endif
+#ifndef PAN_RATE
+#define PAN_RATE 50
+#endif
+#ifndef PAN_CENTER
+#define PAN_CENTER 1500
+#endif
+#ifndef TILT_CENTER
+#define TILT_CENTER 1500
+#endif
+#ifndef TILT_AREA
+#define TILT_AREA 300
+#endif
+#ifndef TILT_DOWN
+#define TILT_DOWN 1800
+#endif
+
+bool_t cam_flag;
+bool_t servo_flag;
+#ifdef CAM_SETUP
+int16_t pan_center = PAN_CENTER;
+int16_t tilt_coeff = TILT_COEFF;
+int16_t pan_coeff = PAN_COEFF;
+int16_t tilt_rate = TILT_RATE;
+int16_t pan_rate = PAN_RATE;
+int16_t tilt_down = TILT_DOWN;
+int16_t tilt_area = TILT_AREA;
+#endif
+int16_t rotorcraft_cam_tilt = 0;
+int16_t rotorcraft_cam_pan = 0;
+int32_t radio_cam_value = 0;
+
+void krooz_cam_init() {
+  ActuatorsPwmInit();
+//  kamera_init();
+}
+
+void krooz_cam_periodic() {
+  cam_flag = TRUE;
+}
+
+void krooz_servo_periodic() {
+  servo_flag = TRUE;
+}
+
+void krooz_cam_event(void) {
+  if(servo_flag) {
+#ifdef CAM_SETUP
+    int32_t angle_buf = stateGetNedToBodyEulers_i()->phi * pan_coeff / INT32_ANGLE_PI;
+    int32_t rate_buf = stateGetBodyRates_i()->p * pan_rate/INT32_ANGLE_PI;
+    rotorcraft_cam_pan = (int16_t)angle_buf + (int16_t)rate_buf + pan_center;
+    angle_buf = stateGetNedToBodyEulers_i()->theta * tilt_coeff / INT32_ANGLE_PI;
+    rate_buf = stateGetBodyRates_i()->q * tilt_rate / INT32_ANGLE_PI;
+#else
+    int32_t angle_buf = stateGetNedToBodyEulers_i()->phi * PAN_COEFF / INT32_ANGLE_PI;
+    int32_t rate_buf = stateGetBodyRates_i()->p * PAN_RATE / INT32_ANGLE_PI;
+    rotorcraft_cam_pan = (int16_t)angle_buf + (int16_t)rate_buf + PAN_CENTER;
+    angle_buf = stateGetNedToBodyEulers_i()->theta * TILT_COEFF / INT32_ANGLE_PI;
+    rate_buf = stateGetBodyRates_i()->q * TILT_RATE / INT32_ANGLE_PI;
+#endif
+    if(radio_control.status == RC_OK)
+      radio_cam_value = (radio_cam_value*49 + (int32_t)radio_control.values[RADIO_CAM]) / 50;
+    if(autopilot_mode != AP_MODE_NAV)
+#if CAM_SETUP == 1
+      rotorcraft_cam_tilt = (int16_t)angle_buf + (radio_cam_value - MAX_PPRZ) * tilt_area/MAX_PPRZ + (int16_t)rate_buf + tilt_down;
+#else
+      rotorcraft_cam_tilt = (int16_t)angle_buf + (radio_cam_value - MAX_PPRZ) * TILT_AREA/MAX_PPRZ + (int16_t)rate_buf + TILT_DOWN;
+#endif
+    else
+#if CAM_SETUP == 1
+      rotorcraft_cam_tilt = (int16_t)angle_buf + tilt_down + (int16_t)rate_buf;
+#else
+      rotorcraft_cam_tilt = (int16_t)angle_buf + TILT_DOWN + (int16_t)rate_buf;
+#endif
+#ifdef CAM_TEST
+    rotorcraft_cam_tilt = (int16_t)angle_buf + radio_cam_value*600/MAX_PPRZ + (int16_t)rate_buf + PAN_CENTER;
+#endif
+/*
+#ifdef COMMAND_CAM_PAN
+    commands[COMMAND_CAM_PAN] = cam_pan;
+#endif
+#ifdef COMMAND_CAM_TILT
+    /commands[COMMAND_CAM_TILT] = cam_tilt;
+#endif
+*/  
+    Bound(rotorcraft_cam_tilt, 900, 2100);
+    Bound(rotorcraft_cam_pan, 900, 2100);
+    ActuatorPwmSet(8, rotorcraft_cam_tilt);
+    ActuatorPwmSet(9, rotorcraft_cam_pan);
+    ActuatorsPwmCommit();
+    servo_flag = FALSE;
+  }
+  
+  if(cam_flag) {
+  //  periodic_task_kamera();
+    cam_flag = FALSE;
+  }
+}
+
